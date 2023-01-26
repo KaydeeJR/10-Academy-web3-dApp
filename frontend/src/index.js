26bdd4f9e95de8099c23cdc4e3a59f5e1d0f7c15
JANEROSE <janerosenyams@gmail.com>
2023-01-26 12:26:26 +0300

    added login page to frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import {MuiThemeProvider} from '@mui/styles';
+// import {ThemeProvider, withThemeCreator} from '@material-ui/styles';
+// import { createTheme } from '@material-ui/core';
+
+
+// const muiTheme = withThemeCreator();
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(<App/>)
+// const rootElement =
+//     document.getElementById('root');
+// const root = createRoot(rootElement);
+
+// root.render(
+//     <ThemeProvider theme={muiTheme}>
+//         <App />
+//     </ThemeProvider>,
+// );
+// ReactDOM.render(
+//   <ThemeProvider theme={muiTheme}>
+//     <App />
+//   </ThemeProvider>,
+//   document.getElementById('root')
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
